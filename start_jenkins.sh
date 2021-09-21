a02683c15c76d8f3e7054f1b46d8113c04c35e8c
The Construct <80715513+TheConstructAi@users.noreply.github.com>
2021-09-21 12:02:04 +0100

    Update start_jenkins.sh

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -12,15 +12,13 @@ git clone https://github.com/TheConstructAi/jenkins_demo.git
 
 # Run Jenkins
 cd jenkins_demo
-JENKINS_HOME=. java -jar jenkins.war &
+JENKINS_HOME=. java -jar jenkins.war --prefix="/$SLOT_PREFIX/jenkins/" &
 JENKINS_PID=$!
 sleep 15s
 
 # Call the jenkins job
 INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-echo "$INSTANCE_ID"
-URL=`echo "http://$INSTANCE_ID.robotigniteacademy.com:$SLOT_JENKINS_PORT"`
-echo "$URL/buildByToken/build?job=TestJob&token=J3nkinsT0k3n"
+URL=`echo "https://$INSTANCE_ID.robotigniteacademy.com/$SLOT_PREFIX/jenkins"`
 curl -X POST "$URL/buildByToken/build?job=TestJob&token=J3nkinsT0k3n"
 
 # Wait for 30 secs, kill Jenkins and clean up
