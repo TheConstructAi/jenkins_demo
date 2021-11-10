2a97e2a1ef7bca421b64d0d247fb7abca0ba32b5
The Construct <80715513+TheConstructAi@users.noreply.github.com>
2021-11-10 14:47:59 +0100

    Add new url for jenkins.war file

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y openjdk-8-jre
 cd ~
 JENKINS_FILE="/home/user/jenkins.war"
 if [ ! -f "$JENKINS_FILE" ]; then
-    wget https://updates.jenkins.io/download/war/2.303.1/jenkins.war
+    wget https://get.jenkins.io/war-stable/2.303.3/jenkins.war
 fi
 
 # Jenkins is about to run, but we must check if 
